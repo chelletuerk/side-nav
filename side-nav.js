e11fe22a86d5527d7520d9580c48611903b8121f
chelle tuerk <michelletuerk@gmail.com>
2017-02-11 11:59:45 -0700

    show and hide side nav

--- side-nav.js
+++ side-nav.js
@@ -2,7 +2,27 @@
 
 class SideNav {
   constructor() {
+    this.showButtonEl = document.querySelector('.js-menu-show')
+    this.hideButtonEl = document.querySelector('.js-menu-hide')
+    this.sideNavEl = document.querySelector('.js-side-nav')
 
+    this.showSideNav = this.showSideNav.bind(this)
+    this.hideSideNav = this.hideSideNav.bind(this)
+
+    this.addEventListeners()
+  }
+
+  addEventListeners() {
+    this.showButtonEl.addEventListener('click', this.showSideNav)
+    this.hideButtonEl.addEventListener('click', this.hideSideNav)
+  }
+
+  showSideNav() {
+    this.sideNavEl.classList.add('side-nav--visible')
+  }
+
+  hideSideNav() {
+    this.sideNavEl.classList.remove('side-nav--visible')
   }
 }
 
