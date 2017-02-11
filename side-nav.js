5fc2cb74bd9e3b9d768100ed71d596024c332c6c
chelle tuerk <michelletuerk@gmail.com>
2017-02-11 13:27:57 -0700

    final side nav

--- side-nav.js
+++ side-nav.js
@@ -1,77 +1,120 @@
-'use strict'
+'use strict';
 
 class SideNav {
-  constructor() {
-    this.showButtonEl = document.querySelector('.js-menu-show')
-    this.hideButtonEl = document.querySelector('.js-menu-hide')
-    this.sideNavEl = document.querySelector('.js-side-nav')
-    this.sideNavContainerEl = document.querySelector('.js-side-nav-container')
+  constructor () {
+    this.showButtonEl = document.querySelector('.js-menu-show');
+    this.hideButtonEl = document.querySelector('.js-menu-hide');
+    this.sideNavEl = document.querySelector('.js-side-nav');
+    this.sideNavContainerEl = document.querySelector('.js-side-nav-container');
+    this.detabinator = new Detabinator(this.sideNavContainerEl);
+    this.detabinator.inert = true;
 
-    this.showSideNav = this.showSideNav.bind(this)
-    this.hideSideNav = this.hideSideNav.bind(this)
-    this.blockClicks = this.blockClicks.bind(this)
-    this.onTouchStart = this.onTouchStart.bind(this)
-    this.onTouchMove = this.onTouchMove.bind(this)
-    this.onTouchEnd = this.onTouchEnd.bind(this)
-    this.onTransitionEnd = this.onTransitionEnd.bind(this)
+    this.showSideNav = this.showSideNav.bind(this);
+    this.hideSideNav = this.hideSideNav.bind(this);
+    this.blockClicks = this.blockClicks.bind(this);
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
+    this.onTransitionEnd = this.onTransitionEnd.bind(this);
+    this.update = this.update.bind(this);
 
-    this.startX = 0
-    this.currentX = 0
+    this.startX = 0;
+    this.currentX = 0;
+    this.touchingSideNav = false;
 
-    this.addEventListeners()
+    this.supportsPassive = undefined;
+    this.addEventListeners();
   }
 
-  addEventListeners() {
-    this.showButtonEl.addEventListener('click', this.showSideNav)
-    this.hideButtonEl.addEventListener('click', this.hideSideNav)
-    this.sideNavEl.addEventListener('click', this.hideSideNav)
-    this.sideNavContainerEl.addEventListener('click', this.blockClicks)
-
-    this.sideNavContainerEl.addEventListener('touchstart', this.onTouchStart)
-    this.sideNavContainerEl.addEventListener('touchmove', this.onTouchMove)
-    this.sideNavContainerEl.addEventListener('touchend', this.onTouchEnd)
-  }
-
-  onTouchStart(e) {
-    e.preventDefault()
-    this.startX = e.touches[0].pageX
-    this.currentX = this.startX
-  }
-
-  onTouchMove(e) {
-    this.currentX = e.touches[0].pageX
-    const translateX = Math.min(0, this.currentX - this.startX)
-
-    if(translateX < 0) {
-      e.preventDefault()
+  applyPassive () {
+    if (this.supportsPassive !== undefined) {
+      return this.supportsPassive ? {passive: true} : false;
     }
-    this.sideNavContainerEl.style.transform = `translateX(${translateX}px)`
+    let isSupported = false;
+    try {
+      document.addEventListener('test', null, {get passive () {
+        isSupported = true;
+      }});
+    } catch (e) { }
+    this.supportsPassive = isSupported;
+    return this.applyPassive();
   }
 
-  onTouchEnd(e) {
+  addEventListeners () {
+    this.showButtonEl.addEventListener('click', this.showSideNav);
+    this.hideButtonEl.addEventListener('click', this.hideSideNav);
+    this.sideNavEl.addEventListener('click', this.hideSideNav);
+    this.sideNavContainerEl.addEventListener('click', this.blockClicks);
 
+    this.sideNavEl.addEventListener('touchstart', this.onTouchStart, this.applyPassive());
+    this.sideNavEl.addEventListener('touchmove', this.onTouchMove, this.applyPassive());
+    this.sideNavEl.addEventListener('touchend', this.onTouchEnd);
   }
 
-  blockClicks(e) {
-    e.stopPropagation()
+  onTouchStart (e) {
+    if (!this.sideNavEl.classList.contains('side-nav--visible'))
+      return;
+
+    this.startX = e.touches[0].pageX;
+    this.currentX = this.startX;
+
+    this.touchingSideNav = true;
+    requestAnimationFrame(this.update);
   }
 
-  onTransitionEnd(e) {
-    this.sideNavEl.classList.remove('.side-nav--animatable')
-    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd)
+  onTouchMove (e) {
+    if (!this.touchingSideNav)
+      return;
+
+    this.currentX = e.touches[0].pageX;
   }
 
-  showSideNav() {
-    this.sideNavEl.classList.add('side-nav--visible')
-    this.sideNavEl.classList.add('.side-nav--visible')
-    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd)
+  onTouchEnd (e) {
+    if (!this.touchingSideNav)
+      return;
+
+    this.touchingSideNav = false;
+
+    const translateX = Math.min(0, this.currentX - this.startX);
+    this.sideNavContainerEl.style.transform = '';
+
+    if (translateX < 0) {
+      this.hideSideNav();
+    }
   }
 
-  hideSideNav() {
-    this.sideNavEl.classList.add('side-nav--visible')
-    this.sideNavEl.classList.remove('.side-nav--visible')
-    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd)
+  update () {
+    if (!this.touchingSideNav)
+      return;
+
+    requestAnimationFrame(this.update);
+
+    const translateX = Math.min(0, this.currentX - this.startX);
+    this.sideNavContainerEl.style.transform = `translateX(${translateX}px)`;
+  }
+
+  blockClicks (e) {
+    e.stopPropagation();
+  }
+
+  onTransitionEnd (e) {
+    this.sideNavEl.classList.remove('side-nav--animatable');
+    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd);
+  }
+
+  showSideNav () {
+    this.sideNavEl.classList.add('side-nav--animatable');
+    this.sideNavEl.classList.add('side-nav--visible');
+    this.detabinator.inert = false;
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
+  }
+
+  hideSideNav () {
+    this.sideNavEl.classList.add('side-nav--animatable');
+    this.sideNavEl.classList.remove('side-nav--visible');
+    this.detabinator.inert = true;
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
   }
 }
 
-new SideNav()
+new SideNav();
