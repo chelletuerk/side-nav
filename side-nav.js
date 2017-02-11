9619b2edee7795d7ed45a5bcd691462f4e3e6cd5
chelle tuerk <michelletuerk@gmail.com>
2017-02-11 13:03:49 -0700

    swipeable in mobile form

--- side-nav.js
+++ side-nav.js
@@ -10,6 +10,13 @@ class SideNav {
     this.showSideNav = this.showSideNav.bind(this)
     this.hideSideNav = this.hideSideNav.bind(this)
     this.blockClicks = this.blockClicks.bind(this)
+    this.onTouchStart = this.onTouchStart.bind(this)
+    this.onTouchMove = this.onTouchMove.bind(this)
+    this.onTouchEnd = this.onTouchEnd.bind(this)
+    this.onTransitionEnd = this.onTransitionEnd.bind(this)
+
+    this.startX = 0
+    this.currentX = 0
 
     this.addEventListeners()
   }
@@ -19,18 +26,51 @@ class SideNav {
     this.hideButtonEl.addEventListener('click', this.hideSideNav)
     this.sideNavEl.addEventListener('click', this.hideSideNav)
     this.sideNavContainerEl.addEventListener('click', this.blockClicks)
+
+    this.sideNavContainerEl.addEventListener('touchstart', this.onTouchStart)
+    this.sideNavContainerEl.addEventListener('touchmove', this.onTouchMove)
+    this.sideNavContainerEl.addEventListener('touchend', this.onTouchEnd)
+  }
+
+  onTouchStart(e) {
+    e.preventDefault()
+    this.startX = e.touches[0].pageX
+    this.currentX = this.startX
+  }
+
+  onTouchMove(e) {
+    this.currentX = e.touches[0].pageX
+    const translateX = Math.min(0, this.currentX - this.startX)
+
+    if(translateX < 0) {
+      e.preventDefault()
+    }
+    this.sideNavContainerEl.style.transform = `translateX(${translateX}px)`
+  }
+
+  onTouchEnd(e) {
+
   }
 
   blockClicks(e) {
     e.stopPropagation()
   }
 
+  onTransitionEnd(e) {
+    this.sideNavEl.classList.remove('.side-nav--animatable')
+    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd)
+  }
+
   showSideNav() {
     this.sideNavEl.classList.add('side-nav--visible')
+    this.sideNavEl.classList.add('.side-nav--visible')
+    this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd)
   }
 
   hideSideNav() {
-    this.sideNavEl.classList.remove('side-nav--visible')
+    this.sideNavEl.classList.add('side-nav--visible')
+    this.sideNavEl.classList.remove('.side-nav--visible')
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd)
   }
 }
 
